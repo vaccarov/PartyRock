4bc386e0b74b2a86b8e76cf438c2f0f9252bb7cf
Victor Vaccaro <victor.vaccaro@ibm.com>
2017-04-02 10:32:00 +0200

    first commit

---
+++ PartyRock/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  PartyRock
+//
+//  Created by VACCARO on 31/03/2017.
+//  Copyright © 2017 VACCARO. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PartyRock/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  PartyRock
+//
+//  Created by VACCARO on 31/03/2017.
+//  Copyright © 2017 VACCARO. All rights reserved.
+//
+
+import Foundation
